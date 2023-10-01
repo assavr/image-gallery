3b0cbd82995188788659fc36b099dc94f0996a34
Anastasia Savrukhina <assavr0@gmail.com>
2023-10-01 19:56:53 +0300

    feat: add settings files

--- image-gallery/index.js
+++ image-gallery/index.js
@@ -1,14 +1,12 @@
-const urlDefault = 'https://api.unsplash.com/search/photos?per_page=15&client_id=20YMWruSfryYAX6ydVUA7oHD71nN4Zu4J2sSnp-afwI&query=cats';
-const urlForSearch = 'https://api.unsplash.com/search/photos?per_page=15&client_id=20YMWruSfryYAX6ydVUA7oHD71nN4Zu4J2sSnp-afwI&query=';
-const main = document.querySelector('.main')
-const mainImg = document.querySelector('.main-img');
-const alertBlock = document.querySelector('.alert-block')
-const form = document.querySelector('.form');
-const search = document.querySelector('.search');
+const urlDefault =
+  'https://api.unsplash.com/search/photos?page=1&per_page=20&client_id=20YMWruSfryYAX6ydVUA7oHD71nN4Zu4J2sSnp-afwI&query=random';
+const urlForSearch =
+  'https://api.unsplash.com/search/photos?page=1&per_page=20&client_id=20YMWruSfryYAX6ydVUA7oHD71nN4Zu4J2sSnp-afwI&query=';
+const alertBlock = document.querySelector('.alert-block');
 const btnSearch = document.querySelector('.btn-search');
-const wrapper = document.querySelector('.wrapper')
-const textAlert = document.querySelector('.text-alert')
-
+const form = document.querySelector('.form');
+const mainImg = document.querySelector('.main-img');
+const search = document.querySelector('.search');
 // get data from API
 async function getData(url) {
   const res = await fetch(url);
@@ -17,7 +15,6 @@ async function getData(url) {
   showData(data);
   hasInvalidSearch(isEmpty);
 }
-
 
 // show data on page
 function showData(data) {
@@ -29,38 +26,36 @@ function showData(data) {
     const img = document.createElement('img');
     const btnDownload = document.createElement('button');
     const link = document.createElement('a');
-    const iconDownload = document.createElement('img')
+    const iconDownload = document.createElement('img');
     blockImg.classList.add('block-img');
     img.classList.add('img');
     mainImg.append(blockImg);
     blockImg.append(img);
     img.setAttribute('alt', 'cats');
+    img.src = e.urls.thumb;
     img.src = e.urls.regular;
     link.href = e.links.download;
-    link.setAttribute('target', '_blank')
+    link.setAttribute('target', '_blank');
     iconDownload.src = './src/img/download-icon.svg';
-    iconDownload.classList.add('icon-download')
-    btnDownload.classList.add('btn-download')
+    iconDownload.classList.add('icon-download');
+    btnDownload.classList.add('btn-download');
     blockImg.addEventListener('mouseover', () => {
-      setTimeout( () => {
-      blockImg.append(btnDownload);
-      btnDownload.append(link);
-      link.append(iconDownload);
-      img.classList.add('blur')
-      }, 600) 
-    })
+      setTimeout(() => {
+        blockImg.append(btnDownload);
+        btnDownload.append(link);
+        link.append(iconDownload);
+        img.classList.add('blur');
+      }, 600);
+    });
 
     blockImg.addEventListener('mouseout', () => {
-      setTimeout( () => {
+      setTimeout(() => {
         btnDownload.remove();
-        img.classList.remove('blur')
-
-        }, 1300)
-    })
-
-  })
+        img.classList.remove('blur');
+      }, 1300);
+    });
+  });
 }
-
 
 // submit query
 function submitQuery(e) {
@@ -74,12 +69,16 @@ function submitQuery(e) {
 form.addEventListener('submit', submitQuery);
 btnSearch.addEventListener('click', submitQuery);
 
-
 // checking for invalid value
 function hasInvalidSearch(isEmpty) {
   if (isEmpty === 0) {
     alertBlock.classList.add('height100');
-    alertBlock.innerHTML = '<p class="text-alert change">Nothing was found for the query' + ' "' + search.value + '".' + 'Try again...</p>';
+    alertBlock.innerHTML =
+      '<p class="text-alert change">Nothing was found for the query' +
+      ' "' +
+      search.value +
+      '".' +
+      'Try again...</p>';
   }
 }
 getData(urlDefault);
